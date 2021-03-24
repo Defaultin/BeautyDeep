d4d53d5ae73e25ef193539462e18bb23e8b904d0
Kirill <gorynovich0101@gmail.com>
2021-03-24 20:27:35 +0300

    v3.0.0

--- web-server/setup.sh
+++ web-server/setup.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get -y install python3 python3-pip git
 git clone https://github.com/Defaultin/BeautyDeep.git
 pip3 install -r BeautyDeep/web-server/requirements.txt
-python3 BeautyDeep/web-server/server.py
+python3 BeautyDeep/web-server/ngrok.py
